8658015e730ee4721179040d2d6d659b4af1ceb4
Daniel Tolbert <dtolbert@bandwidth.com>
2018-03-29 18:35:33 -0400

    working

--- index.js
+++ index.js
@@ -10,20 +10,21 @@ const download = (url, dest) => {
     method: 'get',
   }).then((result) => {
     fs.writeFileSync(dest, result.data);
-    return outputFilename;
+    return dest;
   })
   .catch(e => {
+    console.log(e)
     console.log('error saving url');
   });
 }
 
-hooks.init = function() {
-  console.log('hi');
-  var urls = this.options.pluginsConfig && this.options.pluginsConfig.saveUrl && this.options.pluginsConfig.saveUrl;
-  for (i = 0; i < urls.length; i++) {
-    let fileName = download(urls[i].url, urls[i].dest);
-    console.log(`Saved ${fileName}`);
-  }
+module.exports = {
+  hooks: {
+    "init": function() {
+        var urls = this.options.pluginsConfig && this.options.pluginsConfig.saveUrls && this.options.pluginsConfig.saveUrls.urls;
+        for (i = 0; i < urls.length; i++) {
+          let fileName = download(urls[i].url, urls[i].dest);
+        }
+      }
+    }
 };
-
-module.exports = hooks;
